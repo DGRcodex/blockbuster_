1a53436acbc2bb08be13cca3d78b2fc79fcb8f0d
Hugo Caballero <hcaballero.docencia@gmail.com>
2023-06-29 21:55:36 -0400

    Carga de solo Movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# i = 1
-# 50.times do
-#     Movie.create!([
-#       { name: Faker::Movie.title, client_id: i }
-#     ])
-#     i = i + 1
-#   end
+ i = 1
+ 50.times do
+     Movie.create!([
+       { name: Faker::Movie.title, client_id: i }
+     ])
+     i = i + 1
+   end
 
-  10.times do
-    Client.create!([
-      { name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 100) }
-    ])
-  end
+#   10.times do
+#     Client.create!([
+#       { name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 100) }
+#     ])
+#   end
